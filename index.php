486b25c8e4e8f1429e1b431619cb8776a4449ad6
jaiber430 <jaiber4302@gmail.com>
2025-10-30 00:28:29 -0500

    Algunos estilos

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@500;700;900&family=Press+Start+2P&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style4.css">
     <script src="main.js" defer></script>
     <title>English Interactive Game</title>
     <!-- <style>
